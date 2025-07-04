652845b06cc0fbc21e35d8f312310ac6c0ee44b3
PracticeMedicine <practicemedicine3@gmail.com>
2025-07-04 21:59:18 +0800

    str: update UtlString class

--- src/Libraries/AridityTeam.Platform.Core/Text/UtlString.cs
+++ src/Libraries/AridityTeam.Platform.Core/Text/UtlString.cs
@@ -6,7 +6,7 @@ namespace AridityTeam.Text;
 /// <summary>
 /// 
 /// </summary>
-public class UtlString
+public class UtlString : IEquatable<UtlString>
 {
     private string _value = string.Empty;
     private int _allocSize;
@@ -50,4 +50,27 @@ public class UtlString
 
     /// <inheritdoc/>
     public override string ToString() => _value;
+
+    /// <inheritdoc/>
+    public bool Equals(UtlString? other) => _value == other?._value;
+
+    /// <summary>
+    /// Determines whether the current string contains the specified substring.
+    /// </summary>
+    /// <param name="substring">The substring to locate within the current string. Cannot be <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if the specified substring occurs within the current string; otherwise, <see
+    /// langword="false"/>.</returns>
+    public bool Contains(string? substring) => substring != null && _value.Contains(substring);
+
+    /// <summary>
+    /// Implicitly converts a <see cref="UtlString"/> instance to its underlying string value.
+    /// </summary>
+    /// <param name="utlString">The <see cref="UtlString"/> instance to convert.</param>
+    public static implicit operator string(UtlString utlString) => utlString._value;
+
+    /// <summary>
+    /// Implicitly converts a <see cref="UtlString"/> instance to its underlying string value.
+    /// </summary>
+    /// <param name="v">The string the value to set.</param>
+    public static implicit operator UtlString(string v) => new UtlString(v.Length) { _value = v };
 }
---
+++ src/Tests/AridityTeam.Platform.Tests/Text/UtlStringTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AridityTeam.Text;
+
+namespace AridityTeam.Platform.Tests.Text;
+
+public class UtlStringTests
+{
+    [Fact]
+    public void UtlString_StringContains()
+    {
+        UtlString str = "Hello, World!";
+        string substring = "World";
+        bool result = str.Contains(substring);
+        Assert.True(result, $"Expected '{str}' to contain '{substring}'");
+    }
+
+    [Fact]
+    public void UtlString_ReserveIncreasesCapacity()
+    {
+        UtlString str = new UtlString(10);
+        str.Append("Hello");
+        int initialLength = str.ToString().Length;
+        str.Reserve(20);
+        str.Append(", World!");
+        
+        Assert.True(str.ToString().Length > initialLength, "Expected string length to increase after reserve and append.");
+    }
+}
